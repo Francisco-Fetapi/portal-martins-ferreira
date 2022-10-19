9d6155ea41140f412e86e23630f03a1793bf984f
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-10-19 06:52:11 +0100

    feat: page signup created

---
+++ src/pages/criar-conta.tsx
@@ -0,0 +1,16 @@
+import { Center } from "@mantine/core";
+import Head from "next/head";
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <Head>
+        <title>Criar conta</title>
+      </Head>
+      <Center sx={{ height: "100vh" }}>
+        <h1>Ola Mundo</h1>
+      </Center>
+    </>
+  );
+}
