48f125ae1d6c484310668a43d8adf2c34ad1ee5a
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-11-10 12:20:41 +0100

    feat: more items at navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import {
   TextInput,
   Stack,
 } from "@mantine/core";
-import { IconHome, IconSearch } from "@tabler/icons";
+import { IconHome, IconNotes, IconPaperBag, IconSearch, IconUser } from "@tabler/icons";
 import React from "react";
 import NavBarLink from "./NavBarLink";
 import { UserButton } from "./UserButton";
@@ -28,7 +28,9 @@ export default function NavBar({ opened }: NavBarProps) {
             rightSectionWidth={70}
             mb="sm"
           />
-          <NavBarLink Icon={IconHome} label="Home" link="/" />
+          <NavBarLink Icon={IconHome} label="Página Inicial" link="/" />
+          <NavBarLink Icon={IconUser} label="Perfil" link="/" />
+          <NavBarLink Icon={IconNotes} label="Guardadas" link="/" />
         </Stack>
       </Navbar.Section>
       <Navbar.Section>
