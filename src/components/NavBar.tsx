e9e0f0b739144d3594bdb401c5f19c4e7b714bae
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-11-10 10:43:51 +0100

    feat: search input putted on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,12 @@
-import { Navbar, Text, ScrollArea } from "@mantine/core";
-import { IconHome } from "@tabler/icons";
+import {
+  Navbar,
+  Divider,
+  Text,
+  ScrollArea,
+  TextInput,
+  Stack,
+} from "@mantine/core";
+import { IconHome, IconSearch } from "@tabler/icons";
 import React from "react";
 import NavBarLink from "./NavBarLink";
 import { UserButton } from "./UserButton";
@@ -13,9 +20,19 @@ export default function NavBar({ opened }: NavBarProps) {
     <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
       <Navbar.Section grow component={ScrollArea} p="md">
         {/* Os itens do menu */}
-        <NavBarLink Icon={IconHome} label="Home" link="/" />
+        <Stack>
+          <TextInput
+            placeholder="Procurar noticias"
+            size="xs"
+            icon={<IconSearch size={12} stroke={1.5} />}
+            rightSectionWidth={70}
+            mb="sm"
+          />
+          <NavBarLink Icon={IconHome} label="Home" link="/" />
+        </Stack>
       </Navbar.Section>
       <Navbar.Section>
+        <Divider />
         <UserButton
           name="Nome do usuario"
           email="emaildousuario@gmail.com"
