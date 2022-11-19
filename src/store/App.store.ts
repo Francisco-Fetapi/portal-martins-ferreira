6287779c62a849e3bfc4ebca9831be5adaad1df8
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-11-19 12:22:26 +0100

    refactor: re-extructer redux scheme

--- src/store/App.store.ts
+++ src/store/App.store.ts
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 import { IUser, IUserLogged } from "../interfaces/IUser";
-import { reducers } from "./App.actions";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import useStatePersist from "../hooks/useStatePersist";
+import { stateReseted } from "./utils";
 
 export const THEME_KEY_IN_LOCALSTORAGE = "darkMode";
 export const SIGNUP_KEY_IN_LOCALSTORAGE = "signup-data";
@@ -32,7 +33,58 @@ export function sliceCreator(initialState: App) {
   return createSlice({
     name: "app",
     initialState,
-    reducers,
+    reducers: {
+      toggleTheme(state) {
+        state.darkMode = !state.darkMode;
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { save } = useStatePersist<boolean>(THEME_KEY_IN_LOCALSTORAGE);
+        save(state.darkMode);
+      },
+      setTheme(state, action: PayloadAction<boolean>) {
+        state.darkMode = action.payload;
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { save } = useStatePersist<boolean>(THEME_KEY_IN_LOCALSTORAGE);
+        save(state.darkMode);
+      },
+      setSignUpData(
+        state,
+        action: PayloadAction<Partial<IUserFormSigninData>>
+      ) {
+        Object.assign(state.signupData, action.payload);
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { save } = useStatePersist<Partial<IUserFormSigninData>>(
+          SIGNUP_KEY_IN_LOCALSTORAGE
+        );
+        save(state.signupData);
+      },
+      setUserLoggedData(state, action: PayloadAction<IUserLogged>) {
+        state.userLoggedData = action.payload;
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        if (state.userLoggedData) {
+          setCookie(
+            null,
+            USER_COOKIE_KEY,
+            JSON.stringify(state.userLoggedData),
+            {
+              maxAge: 30 * 24 * 60 * 60,
+              path: "/",
+            }
+          );
+        }
+      },
+      logout(state) {
+        state.userLoggedData = {} as IUserLogged;
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        destroyCookie(null, USER_COOKIE_KEY);
+      },
+      resetAllState(state, action: PayloadAction<boolean>) {
+        if (action.payload) {
+          Object.assign(state, initialState);
+          return;
+        }
+        Object.assign(state, stateReseted(initialState));
+      },
+    },
   });
 }
 
