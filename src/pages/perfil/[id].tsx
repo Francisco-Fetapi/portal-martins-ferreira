8f9b4ed2f3df4b47234f34cc60be05b612e8711d
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-11-26 08:18:43 +0100

    feat: show another profile page

--- src/pages/perfil/[id].tsx
+++ src/pages/perfil/[id].tsx
@@ -3,45 +3,73 @@ import { UserInfo } from "../../components/UserInfo";
 import { Text, Box, Title } from "@mantine/core";
 import PostArea from "../../components/PostArea";
 import ArticlesList from "../../components/ArticlesList";
-import { redirectIfNoUser } from "../../helpers/redirectIfNoUser";
+import { getToken, redirectIfNoUser } from "../../helpers/redirectIfNoUser";
 import { IUserLogged } from "../../interfaces/IUser";
+import { GetServerSideProps } from "next";
+import { strapi } from "../../api/strapi";
 
 interface PageProps {
   user: IUserLogged;
   otherUser: IUserLogged;
 }
 
-export default function ProfileUserPage() {
+export default function ProfileUserPage({ otherUser }: PageProps) {
   return (
     <AppScheme>
-      <UserInfo
-        user={{
-          username: "Nome do Usuario",
-          phoneNumber: "934312217",
-          photo: null,
-          email: "emaildousuario@gmail.com",
-          genre: "m",
-          myCourse: "Nome do Curso",
-          birthday: new Date().toLocaleDateString(),
-          myGlade: 12,
-          myClass: "Turma 2",
-        }}
-        isMine={false}
-      />
+      {otherUser ? (
+        <>
+          <UserInfo user={otherUser} isMine={false} />
 
-      <Box mt={30}>
-        <Title order={2}>Noticias</Title>
-        <Text color="dimmed" size="xs" mt={5}>
-          Todas as noticias publicadas por <b>Nome do Usuario</b> são exibidas
-          nesta seção desde a mais recente à mais antiga.
-        </Text>
-      </Box>
+          <Box mt={30}>
+            <Title order={2}>Noticias</Title>
+            <Text color="dimmed" size="xs" mt={5}>
+              Todas as noticias publicadas por <b>Nome do Usuario</b> são
+              exibidas nesta seção desde a mais recente à mais antiga.
+            </Text>
+          </Box>
 
-      <Box mt={30}>
-        <ArticlesList />
-      </Box>
+          <Box mt={30}>
+            <ArticlesList />
+          </Box>
+        </>
+      ) : (
+        <p>Nenhum usuario encontrado</p>
+      )}
     </AppScheme>
   );
 }
 
-export const getServerSideProps = redirectIfNoUser;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const token = getToken(ctx);
+  const isNotLogged = !token;
+
+  if (isNotLogged) {
+    return redirectIfNoUser(ctx);
+  }
+
+  try {
+    const userId = ctx.query.id;
+
+    const { data: user } = await strapi.get<IUserLogged>(
+      `/users/${userId}?populate=photo`,
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+
+    return {
+      props: {
+        user,
+      },
+    };
+  } catch (e: any) {
+    console.error(e.message);
+    return {
+      props: {
+        otherUser: undefined,
+      },
+    };
+  }
+};
