ed7fb58d2ca84f7da6fe1e5d51953011af3f4b79
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-12-05 12:41:12 +0100

    feat: app scheme simple created

--- src/pages/bloqueado.tsx
+++ src/pages/bloqueado.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { Center, Title, Text, Box } from "@mantine/core";
+import AppSchemeSimple from "../components/AppSchemeSimple";
 
 export default function BlockedPage() {
-  return <div>bloqueado</div>;
+  return (
+    <AppSchemeSimple>
+      <Center
+        sx={{
+          minHeight: "90vh",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 500,
+          }}
+        >
+          <Title align="center" order={2}>
+            Você foi bloqueado!
+          </Title>
+          <Text align="center" size="xs" color="dimmed">
+            Por alguma razão você foi bloqueado pelo Administrador. A partir de
+            agora não poderá mais acessar o portal a menos que seja
+            desbloqueado.
+          </Text>
+        </Box>
+      </Center>
+    </AppSchemeSimple>
+  );
 }
